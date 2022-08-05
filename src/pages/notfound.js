8dad664df6a4ec578d5b57dc56996ed6928de09c
choudharysidharth082000 <choudharysidharth082000@gmail.com>
2022-08-06 00:24:00 +0530

    added comming soon page

--- src/pages/notfound.js
+++ src/pages/notfound.js
@@ -6,8 +6,8 @@ export default function notfound() {
     <>
       <div className="error-page vh-100 d-flex justify-content-center text-center">
         <div className="my-auto">
-          <h2>404</h2>
-          <p>Oops something went wrong</p>
+          <h3>Comming Soon</h3>
+          <p>This page is under progress...</p>
           <Link to="/" className="btn">
             Back to Home <i className="icon ion-md-home"></i>
           </Link>
